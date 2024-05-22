497aeec5309ae2bb72a9b8f77cfdfdda314d16f2
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-05-22 17:54:57 +0000

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="card">
+      <h1>Java</h1>
+      < img src= "https://salvatore.academy/devmon/1_java.png" width="100"/>
+     </div>
+    </>
+  )
+}
+
+export default App
